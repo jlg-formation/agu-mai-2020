61577506f7a4e841f98a073e0edf5f7dc6bff894
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-27 16:10:29 +0200

    change local to french

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,21 +12,19 @@ import { LegalComponent } from './routes/legal/legal.component';
 import { StockComponent } from './routes/stock/stock.component';
 import { CreateComponent } from './routes/create/create.component';
 
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LegalComponent,
     StockComponent,
-    CreateComponent
+    CreateComponent,
   ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    LayoutModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule, LayoutModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
