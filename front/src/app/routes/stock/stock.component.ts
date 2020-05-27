38a36485e84cb8dcdf21676d8ed3a4582439571c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-27 10:50:41 +0200

    make array dynamic

--- front/src/app/routes/stock/stock.component.ts
+++ front/src/app/routes/stock/stock.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles = [
+    { name: 'Tournevis', price: 12.34, qty: 4 },
+    { name: 'Marteau', price: 12.34, qty: 14 },
+    { name: 'Clous', price: 12.34, qty: 24 },
+    { name: 'Pince', price: 12.34, qty: 41 },
+    { name: 'Scie', price: 12.34, qty: 45 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
