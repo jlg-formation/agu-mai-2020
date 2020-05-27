3c8688059888e63e2e33026139d7a3f54e1a123b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-27 14:07:48 +0200

    add article works

--- front/src/app/routes/stock/stock.component.ts
+++ front/src/app/routes/stock/stock.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-stock',
@@ -7,15 +8,7 @@ import { Article } from 'src/app/interfaces/article';
   styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 12.34, qty: 4 },
-    { name: 'Marteau', price: 12.34, qty: 14 },
-    { name: 'Clous', price: 12.34, qty: 24 },
-    { name: 'Pince', price: 12.34, qty: 41 },
-    { name: 'Scie', price: 12.34, qty: 23 },
-  ];
-
-  constructor() {}
+  constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
 }
--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticleService {
+  articles: Article[] = [
+    { name: 'Tournevis', price: 12.34, qty: 4 },
+    { name: 'Marteau', price: 12.34, qty: 14 },
+    { name: 'Clous', price: 12.34, qty: 24 },
+    { name: 'Pince', price: 12.34, qty: 41 },
+    { name: 'Scie', price: 12.34, qty: 23 },
+  ];
 
-
-  constructor() { }
+  constructor() {}
 
   create(article: Article) {
     console.log('article: ', article);
-
+    this.articles.push(article);
   }
 }
