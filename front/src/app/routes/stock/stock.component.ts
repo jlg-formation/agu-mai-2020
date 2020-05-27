3afc8238e6cf2605d6771fcbcaafe4c0ad6f1f09
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-27 15:11:35 +0200

    added selectedArticles

--- front/src/app/routes/stock/stock.component.ts
+++ front/src/app/routes/stock/stock.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Article } from 'src/app/interfaces/article';
 import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-stock',
@@ -8,11 +8,28 @@ import { ArticleService } from 'src/app/services/article.service';
   styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+
+  selectedArticles: Article[] = [];
+
   constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
 
-  select() {
+  select(evt: MouseEvent, article: Article) {
+    console.log('evt: ', evt);
     console.log('select');
+    const elt = evt.target as HTMLElement;
+    const row = elt.parentElement;
+    console.log('row: ', row);
+    if (row.classList.contains('selected')) {
+      row.classList.remove('selected');
+      const index = this.selectedArticles.findIndex(a => a === article);
+      this.selectedArticles.splice(index, 1);
+    } else {
+      row.classList.add('selected');
+      this.selectedArticles.push(article);
+    }
+    console.log('this.selectedArticles: ', this.selectedArticles);
+
   }
 }
