77f238b230a8a176d81b95cd8b22d42568de6dcd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-27 11:48:35 +0200

    add formgroup

--- front/src/app/routes/create/create.component.ts
+++ front/src/app/routes/create/create.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  f = new FormGroup({});
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
