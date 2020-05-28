e92501ff01dff64e53c2c5abff8847c9d5ca31cd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-28 11:42:12 +0200

    added article webservice

--- back/ws.ts
+++ back/ws.ts
@@ -8,3 +8,12 @@ app.get("/now", (req, res) => {
     date: new Date(),
   });
 });
+
+app.get("/articles", (req, res) => {
+  res.json([
+    { name: "Tournevis", price: 12.23, qty: 5 },
+    { name: "Marteau", price: 5.6, qty: 11 },
+    { name: "Scie", price: 34.7, qty: 3 },
+    { name: "Pince coupante", price: 10.99, qty: 3 },
+  ]);
+});
