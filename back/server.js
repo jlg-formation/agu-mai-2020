d1e131d13fadb03e7c1fe9366dd68d160585b53e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-28 10:44:29 +0200

    add web service date

--- back/server.js
+++ back/server.js
@@ -5,12 +5,18 @@ const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-    console.log('req.url', req.url);
-    next();
+  console.log("req.url", req.url);
+  next();
 });
 
-app.use(express.static('public'));
-app.use(serveIndex('public'));
+app.get("/ws/now", (req, res) => {
+  res.json({
+    date: new Date(),
+  });
+});
+
+app.use(express.static("public"));
+app.use(serveIndex("public"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
