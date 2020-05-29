8ae24cb952a6ba3f21530d658e480dd014a193f7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-29 16:22:18 +0200

    remove log

--- front/src/app/widget/duration.pipe.ts
+++ front/src/app/widget/duration.pipe.ts
@@ -5,9 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DurationPipe implements PipeTransform {
   transform(value: number, ...args: unknown[]): unknown {
-    console.log('value: ', value);
     const minutes = ('' + Math.floor(value / 60)).padStart(2, '0');
-    console.log('minutes: ', minutes);
     const seconds = ('' + (value % 60)).padStart(2, '0');
     return `${minutes}:${seconds}`;
   }
