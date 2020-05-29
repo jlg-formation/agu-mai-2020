e6d8bc84f36854363b36db90dd268458bf682277
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-29 17:10:18 +0200

    added ng test for autofocus

--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,39 @@
-import { AutofocusDirective } from './autofocus.directive';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { WidgetModule } from './widget.module';
+
+@Component({
+  template: '<input type="text" appAutofocus>',
+})
+class TestComponent {
+}
 
 describe('AutofocusDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new AutofocusDirective();
-  //   expect(directive).toBeTruthy();
-  // });
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent],
+      imports: [WidgetModule],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+  it('should instantiate the directive', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should test that the element on focus is an input', () => {
+
+    const compiled = fixture.nativeElement;
+    const input = compiled.querySelector('input') as HTMLElement;
+    expect(document.activeElement === input).toBeTrue();
+  });
 });
